26f1d795f387c8f7e30a4d1a1d6d924508650181
mgt <mgt@oi-wiki.org>
2022-11-03 22:55:31 +0800

    add error handling and timeout

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
+use anyhow::{Context, Result};
 use clap::Parser;
 use serde::{Deserialize, Serialize};
 use std::{
     collections::HashMap,
     fs::{self},
     path::{Path, PathBuf},
-    time::SystemTime,
+    time::{Duration, SystemTime},
 };
-
 const CONFIG_PATH: &str = "/etc/hubakc/config.toml";
 
 #[derive(Parser, Debug)]
@@ -19,6 +19,7 @@ struct Args {
 #[derive(Debug, Serialize, Deserialize)]
 struct Config {
     ttl: u64,
+    timeout: u64,
     cache_folder: String,
     user_map: HashMap<String, String>, // user -> gh user
 }
@@ -29,47 +30,58 @@ impl Default for Config {
             cache_folder: "/tmp/hubakc".to_string(),
             user_map: Default::default(),
             ttl: 600,
+            timeout: 15
         }
     }
 }
 
 impl Config {
-    fn from_path(p: &str) -> Self {
-        toml::de::from_str(fs::read_to_string(p).unwrap().as_str()).unwrap()
+    fn from_path(p: &str) -> Result<Self> {
+        let res: Self = toml::from_str(fs::read_to_string(p)?.as_str())?;
+        Ok(res)
     }
 }
 
-fn get_pubkey_from_gh(gh_user_name: &str) -> String {
-    ureq::get(&format!("https://github.com/{gh_user_name}.keys"))
-        .call()
-        .unwrap()
-        .into_string()
-        .unwrap()
+fn get_pubkey_from_gh(gh_user_name: &str, timeout: u64) -> Result<String> {
+    let res = ureq::get(&format!("https://github.com/{gh_user_name}.keys"))
+        .timeout(Duration::from_secs(timeout))
+        .call()?
+        .into_string()?;
+    Ok(res)
 }
 
-fn main() {
+fn main() -> Result<()> {
     let Args { user_name } = Args::parse();
-    let config = Config::from_path(CONFIG_PATH);
+    let config = Config::from_path(CONFIG_PATH)
+        .with_context(|| format!("failed to opening config file: {CONFIG_PATH}"))?;
     let cache_dir_path = Path::new(&config.cache_folder);
     if !cache_dir_path.exists() {
-        fs::create_dir_all(cache_dir_path).unwrap();
+        fs::create_dir_all(cache_dir_path)
+            .with_context(|| format!("failed to create cache dir: {}", cache_dir_path.display()))?;
     }
     if let Some(gh_user) = config.user_map.get(&user_name) {
         let cache_path = PathBuf::from(format!("{}/{}", config.cache_folder, gh_user));
         if cache_path.exists() {
-            let mod_time = cache_path.metadata().unwrap().modified().unwrap();
-            if SystemTime::now()
-                .duration_since(mod_time)
-                .unwrap()
-                .as_secs()
-                <= config.ttl
+            let mod_time = cache_path
+                .metadata()
+                .with_context(|| format!("failed to read metadata: {}", cache_path.display()))?
+                .modified()?;
+            if SystemTime::now().duration_since(mod_time)?.as_secs() <= config.ttl
+                || get_pubkey_from_gh(gh_user, config.timeout).is_err()
             {
-                print!("{}", fs::read_to_string(cache_path).unwrap());
-                return;
+                print!("{}", fs::read_to_string(cache_path)?);
+                return Ok(());
             }
         }
-        let keys = get_pubkey_from_gh(&gh_user);
+        let keys = get_pubkey_from_gh(gh_user, config.timeout)
+            .with_context(|| format!("failed to get ssh key for {gh_user}"))?;
         print!("{}", keys);
-        fs::write(cache_path, keys).unwrap();
+        fs::write(&cache_path, keys).with_context(|| {
+            format!(
+                "failed to write keys to cache file: {}",
+                cache_path.display()
+            )
+        })?;
     }
+    Ok(())
 }
